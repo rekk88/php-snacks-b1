15515f94b9ec8d0b7b4f56967b32dbe1a0dc0d9a
maicol <maicol.matteoli@gmail.com>
2021-07-26 16:00:51 +0200

    add  : snack2 (qualcosa non torna)

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="snack1.php">Snack 1</a>
+    <a href="snack2.php?name=Gianni&mail=mailfarlocca@indirizzo.com&age=31">Snack 2</a>
+</body>
+</html>
--- snack2.php
+++ snack2.php
@@ -1,6 +1,20 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
 che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+<?php 
 
+$name = $_GET["name"];
+$age = $_GET["age"];
+$mail = $_GET["mail"];
+
+$access = false;
+var_dump($mail);
+var_dump(strpos("@",$mail));
+
+if(strlen($name)>3 && strpos('@',$mail)!==false && strpos('.',$mail)!==false && is_numeric($age) ){
+    $access = true;
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +25,15 @@ che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è
     <title>Mailll!</title>
 </head>
 <body>
-    
+    <p>
+        <?php if($access){
+                echo "accesso consentito";
+                }
+              else{
+                  echo "accesso negato";
+              } 
+        ?>
+
+    </p>    
 </body>
 </html>
